9c3d8987587eb3949ed9a8056dc5aa10d4077963
TUSHAR-hase <thakortushar81315@gmail.com>
2025-05-13 19:01:17 +0530

    add code

--- portfoliyo/src/components/3dpages/herosection.jsx
+++ portfoliyo/src/components/3dpages/herosection.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-
+import yourImage from '../../assets/tushrphoto.jpg';
 
 const Hero = () => {
   const roles = ['Web Developer', 'App Developer'];
@@ -145,7 +145,7 @@ const Hero = () => {
         >
           <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-white/20 group hover:border-white/30 transition-all duration-300">
             <img
-              src='../../assets/tushrphoto.jpg'
+              src={yourImage}
               alt="Your Name"
               className="w-full h-full object-cover transform group-hover:scale-105 transition-all duration-500"
             />
