a5eb46b46c1724296f91382f51d571a233c553cf
TUSHAR-hase <thakortushar81315@gmail.com>
2025-08-03 15:00:55 +0530

    fix: removed Vite from Next.js project

--- portfoliyo/src/app/layout.js
+++ portfoliyo/src/app/layout.js
@@ -5,11 +5,28 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Tushar Thakor",
+  title: "Tushar Thakor - IITRAM Computer Science Student & Developer",
    description:
-    " Tushar Thakor a passionate Computer Science student IITRAM, hackathon winner, and full-stack developer. Experienced in React, Node.js, Flutter, MongoDB, and C++. Creator of innovative projects in women safety, work tracking, cab booking, and kids animation using AI.",
+    " Tushar Thakor a passionate Computer Science student At IITRAM(Institute of Infrastructure, Technology, Research And Management), hackathon winner, and full-stack developer. Experienced in React, Node.js, Flutter, MongoDB, and C++. Creator of innovative projects in women safety, work tracking, cab booking, and kids animation using AI.",
+   keywords: [
+    "Tushar Thakor",
+    "IITRAM",
+    "Institute of Infrastructure, Technology, Research And Management",
+    "Computer Science Student",
+    "Full-stack Developer",
+    "React Developer",
+    "Node.js Developer",
+    "Flutter Developer",
+    "MongoDB",
+    "C++",
+    "Hackathon Winner",
+    "Women Safety Projects",
+    "AI Animation",
+    "Work Tracking App",
+    "Cab Booking App"
+  ].join(", "),
   icons: {
-    icon: "/tushrphoto.jpg", // Make sure favicon.ico exists in the public folder
+    icon: "/latter.png",
   },
 };
 
